588b3a6dd4206eb16575aa8796360547c928454e
ben_helal_wajdi <benhelalwajdi@gmail.com>
2023-03-28 14:04:56 +0000

    done

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
+import 'package:CIBP/CustomDialog.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   @override
@@ -7,6 +12,9 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final user_name = TextEditingController();
+  final password = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +38,7 @@ class _LoginState extends State<Login> {
               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
               padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: user_name,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'E-mail بريد إلكتروني',
@@ -42,6 +51,7 @@ class _LoginState extends State<Login> {
                   left: 15.0, right: 15.0, top: 15, bottom: 0),
               //padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
+                controller: password,
                 obscureText: true,
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
@@ -67,12 +77,52 @@ class _LoginState extends State<Login> {
               decoration: BoxDecoration(
                   color: Colors.green, borderRadius: BorderRadius.circular(20)),
               child: TextButton(
-                onPressed: () {
-                  /*Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (_) => MyHomePage(title: 'title')()));
-                */
+                onPressed: () async {
+                  var url =
+                      "http://stalk.us-east-1.elasticbeanstalk.com/access/signIn";
+                  var body = jsonEncode({
+                    "username": user_name.text.toString(),
+                    "password": password.text.toString(),
+                    "apikey":
+                        "ZFfewRnTK6UZZ7ef9WyUNVK2jy6BtK1mFyb45Kuc0KxUNzlTpTeqLUlb6EWmIpD0I1JkhDo6zmr4ouLCgXNR6LAURz0LqYV9OdNcNOhiv7XnRcNrMwM1gmV6ADTmKdDG"
+                  });
+                  //print("Body: " + body);
+
+                  await http
+                      .post(url,
+                          headers: {"Content-Type": "application/json"},
+                          body: body)
+                      .then((http.Response response) async {
+                    print("Response status : ${response.statusCode}");
+                    print("Response contentLength : ${response.contentLength}");
+                    print("Response headers : ${response.headers}");
+                    print("Response request :${response.request}");
+                    print("Response body : ${response.body}");
+
+                    String body = response.body;
+                    var parsedJson = json.decode(body);
+                    if (parsedJson['success'] as bool == true) {
+                      var user = parsedJson['user'];
+                      print(true);
+                    }
+                    ;
+                  }); /*final bool _isValid = EmailValidator.validate(user_name.text);
+                  if (_isValid == false) {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) => CustomDialog(
+                              title:
+                                  "Endereço de email invalido \n عنوان البريد الإلكتروني غير صالح",
+                              description:
+                                  "يرجى التحقق من عنوان البريد الإلكتروني الخاص بك\n Por favor, verifique seu endereço de e-mail",
+                              primaryButtonText: "Create My Account",
+                              primaryButtonRoute: "/signUp",
+                              secondaryButtonText: "Maybe Later",
+                              secondaryButtonRoute: "/home",
+                            ));
+                  } else {
+                    //http://stalk.us-east-1.elasticbeanstalk.com/access/
+                  }*/
                 },
                 child: Text(
                   'Conecte-se \n تسجيل الدخول',
